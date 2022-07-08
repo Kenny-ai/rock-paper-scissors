7431756ef491e7813c7ab9b9c4ff7d88273e9c17
Kenny-ai <63028105+Kenny-ai@users.noreply.github.com>
2022-07-08 18:49:39 +0100

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'rock-bg': 'linear-gradient(hsl(349, 71%, 52%), hsl(349, 70%, 56%))',
+        'paper-bg': 'linear-gradient(hsl(230, 89%, 62%),hsl(230, 89%, 65%))',
+        'scissors-bg': 'linear-gradient(hsl(39, 89%, 49%), hsl(40, 84%, 53%))',
+      },
+      boxShadow: {
+        
+      }
+    },
+
   },
   plugins: [],
 }
