4efd4e6636b9ac3f652e27426f9e2c07f27ac331
Kenny-ai <kennysoyoye07@gmail.com>
2024-12-31 20:59:15 +0100

    rules button placement fix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ const Button = () => {
 
   const { setShowRules } = useStateContext();
   return (
-    <div className="py-5 lg:py-3">
+    <div className="py-5 lg:py-8">
       <div className="button flex justify-center lg:justify-end md:px-12">
         <button
           className={"uppercase border-2 border-gray-300 rounded-lg px-7 py-1 cursor-pointer select-none active:scale-105 hover:ring-2"}
